3514f69e84c57dab1458b89f90cc5aae4e33e663
aruto-nick <effo.conti329@gmail.com>
2026-07-08 17:13:24 +0900

    7/8-17:00-実装クラスAdminRepositoryImplの実装途中

--- practice/src/main/java/com/example/demo/service/AdminServiceImpl.java
+++ practice/src/main/java/com/example/demo/service/AdminServiceImpl.java
@@ -1,7 +1,25 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AdminServiceImpl {
+import com.example.demo.entity.Admin;
+import com.example.demo.repository.AdminRepository;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+	
+	//AfminRepositoryを利用できるようにするため
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	//AdminServiceインターフェースにて宣言したメソッドの内容記述
+	@Override
+	public void register(Admin admin) {
+		
+		//
+		adminRepository.save(admin);
+	}
+	
 
 }
