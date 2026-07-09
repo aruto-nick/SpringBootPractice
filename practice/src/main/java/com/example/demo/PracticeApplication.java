fa66adc685d9d8fd2cebe6eba5f141df5734f18d
aruto-nick <effo.conti329@gmail.com>
2026-07-09 11:13:00 +0900

    7/9-11:00-Serviceクラスにて暗号化処理の実装＆暗号化ツールの導入

--- practice/src/main/java/com/example/demo/PracticeApplication.java
+++ practice/src/main/java/com/example/demo/PracticeApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication
-public class PracticeApplication {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class} )
+public class PracticeApplication{ 
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
--- practice/src/main/java/com/example/demo/service/AdminServiceImpl.java
+++ practice/src/main/java/com/example/demo/service/AdminServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//暗号化ツールの導入
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Admin;
@@ -13,11 +15,24 @@ public class AdminServiceImpl implements AdminService {
 	@Autowired
 	private AdminRepository adminRepository;
 	
+	//暗号化ツールのリモコン
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	//AdminServiceインターフェースにて宣言したメソッドの内容記述
 	@Override
 	public void register(Admin admin) {
 		
-		//
+		//画面から届いたパスワードを取り出す
+		String rawPassword = admin.getPassword();
+		
+		//暗号化ツールを活用して取り出したパスワードをハッシュ値（解読不能な文字）に変える
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		
+		//setterによってカプセル化されたpassword変数の変更ができる
+		admin.setPassword(encodedPassword);
+		
+		//データをDBに保存
 		adminRepository.save(admin);
 	}
 	
